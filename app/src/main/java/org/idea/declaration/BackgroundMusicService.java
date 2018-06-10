43196b602e425da16707004fd630472071e7a86c
rickwang@cht.com.tw <rickwang@cht.com.tw>
2018-06-10 20:39:23 +0800

    BackgroundMusicService is 'startForeground' now

--- app/src/main/java/org/idea/declaration/BackgroundMusicService.java
+++ app/src/main/java/org/idea/declaration/BackgroundMusicService.java
@@ -1,11 +1,21 @@
 package org.idea.declaration;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.AssetFileDescriptor;
+import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Binder;
+import android.os.Build;
 import android.os.IBinder;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,6 +39,39 @@ public class BackgroundMusicService extends Service {
         super.onCreate();
 
         player = new MediaPlayer();
+
+        String channelId = "";
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            channelId = createNotificationChannel();
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(getString(R.string.app_name));
+//                .setContentText(getString(R.string.play));
+
+        PendingIntent pi = PendingIntent.getActivity(this, 0,
+                new Intent(this, BackgroundMusicActivity.class),
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        builder.setContentIntent(pi);
+
+        Notification n = builder.build();
+
+        startForeground(1, n);
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    String createNotificationChannel() {
+        String channelId = "declaration";
+        String name = "background music";
+        NotificationChannel nc = new NotificationChannel(channelId, name, NotificationManager.IMPORTANCE_NONE);
+        nc.setLightColor(Color.BLUE);
+        nc.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+        NotificationManager nm = getSystemService(NotificationManager.class);
+        nm.createNotificationChannel(nc);
+
+        return channelId;
     }
 
     @Override
